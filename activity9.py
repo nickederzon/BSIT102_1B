f647d71c1369dcf9731836f6a249666615eadccd
nickederzon <nickederzonmagnaye19@gmail.com>
2025-08-22 13:47:02 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,11 @@
+n = 5
+i = 5
+
+c = 6
+k = 5
+username = 'nick'
+password = 'quebrado'
+print((username == 'nick') and (password == 'quebrado'))
+print( (n > i) and (c < k) )
+print( (n > i) or (c < k) )
+print(not(( n > i) or (c < k)))
