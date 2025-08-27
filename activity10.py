00aeda927d3ae20f2b4576789544c7d48548f9e8
nickederzon <nickederzonmagnaye19@gmail.com>
2025-08-27 15:20:32 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "nick"
+password = "katamad"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password):
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENIED")
