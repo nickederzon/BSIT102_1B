95b51a6d022561a323adfda2f0d245d9569c991b
nickederzon <nickederzonmagnaye19@gmail.com>
2025-09-16 16:06:36 +0800

    Update code_challenge2.py

--- code_challenge2.py
+++ code_challenge2.py
@@ -9,30 +9,30 @@ ni8 = 10
 ni9 = 5
 ni10 = 1
 
-denum = eval(input("Enter the amount to deposit --->  "))
+nick = eval(input("Enter the amount to deposit --->  "))
 print ("here is the breakdown of the deposit amount : ")
-# b1 = demun // ni1   
+# b1 = nick // ni1   
 
-b1 = denum // ni1
-denum = denum % ni1
-b2 = denum  // ni2
-denum = denum % ni2
-b3 = denum // ni3
-denum = denum % ni3
-b4 = denum // ni4
-denum = denum % ni4
-b5 = denum // ni5
-denum = denum % ni5
-b6 = denum // ni6
-denum = denum % ni6
-b7 = denum // ni7
-denum = denum % ni7
-b8 = denum // ni8
-denum = denum % ni8
-b9 = denum // ni9
-denum = denum % ni9
-b10 = denum // ni10
-denum = denum  % ni10
+b1 = nick // ni1
+nick = nick % ni1
+b2 = nick  // ni2
+nick = nick % ni2
+b3 = nick // ni3
+nick = nick % ni3
+b4 = nick // ni4
+nick = nick % ni4
+b5 = nick // ni5
+nick = nick % ni5
+b6 = nick // ni6
+nick = nick % ni6
+b7 = nick // ni7
+nick = nick % ni7
+b8 = nick // ni8
+nick = nick % ni8
+b9 = nick // ni9
+nick = nick % ni9
+b10 = nick // ni10
+nick = nick  % ni10
 
 
 print(b1, "-", ni1)
