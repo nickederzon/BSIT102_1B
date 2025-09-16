a0cc859031e9ee62316db38066f2c760471250c2
nickederzon <nickederzonmagnaye19@gmail.com>
2025-09-16 23:58:02 +0800

    Update code_challenge6.py

--- code_challenge6.py
+++ code_challenge6.py
@@ -1,9 +1,9 @@
-#Enter 10 random numbers, then get the summation of all odd numbers
+print("Enter 7 numbers")
 
 sum = 0
 
-for n in range(1, 11, 1):
-	value = int(input('Enter any number --> '))
+for n in range(1, 8, 1):
+	value = eval(input("Enter any number --> "))
 	if value % 2:
 		sum += value 
 print("The sum of all the given odd numbers is", sum)
