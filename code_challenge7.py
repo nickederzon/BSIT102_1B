521ccc9b75772b7732cb9c5da2c71c0daa2eb007
nickederzon <nickederzonmagnaye19@gmail.com>
2025-09-17 11:07:42 +0800

    Update code_challenge7.py

--- code_challenge7.py
+++ code_challenge7.py
@@ -1,3 +1,8 @@
+#Ask the user to enter a starting number
+#USE A FOR LOOP THAT COUNTS FROM A USER-SPECIFIED NUMBER DOWN TO 1
+#PRINT EACH NUMBER ON A NEW LINE
+#THEN PRINT LIFTOFF!
+
 for x in range(5, 0, -1):
 	print(x)
 print("Liftoff!")
