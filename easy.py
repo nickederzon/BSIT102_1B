30054aefb61959ab09be21aa3fc313813fac45d4
nickederzon <nickederzonmagnaye19@gmail.com>
2025-10-13 17:05:21 +0800

    Create easy.py

---
+++ easy.py
@@ -0,0 +1,12 @@
+for n in range(1,8,1):
+	for i in range(7,n,-1):
+		print(" ", end="")
+	for c in range(1,n,1):
+		print("* " , end="")
+	print()
+	
+	
+for n in range(1,6,1):
+	for i in range(1,n+1,1):
+		print(n,end=" ")
+	print()
