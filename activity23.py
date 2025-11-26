17b5b3359f50a7a507315948ba9d63695757db2b
nickederzon <nickederzonmagnaye19@gmail.com>
2025-11-26 12:54:13 +0800

    Add files via upload

---
+++ activity23.py
@@ -0,0 +1,21 @@
+fruit1 = 'banana'
+fruit2 = 'apple'
+
+fruit = ['apple','banana','mango','avocado','longgans']
+
+print(fruit)
+
+print(fruit[3])
+print(fruit[3:5])
+
+fruit.append("lemons")
+print(fruit)
+
+fruit.append("dragon fruit")
+print(fruit)
+
+fruit.insert(3 , "aratilis")
+print(fruit)
+
+fruit.pop()
+print(fruit)
