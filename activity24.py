7625e93908fecfdedad60aabcd5d0da997d7eaed
nickederzon <nickederzonmagnaye19@gmail.com>
2025-12-01 13:41:18 +0800

    Add files via upload

---
+++ activity24.py
@@ -0,0 +1,9 @@
+from activity24_1 import greetperson
+from activity24_1 import personage
+def greet():
+    print("Hi nice to meet you")
+    print("Hope we got along")
+
+greet()
+greetperson("nick")
+personage("19")
