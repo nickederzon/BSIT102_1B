4387f72956920b43907d20005307a350c1e7e50d
nickederzon <nickederzonmagnaye19@gmail.com>
2026-02-12 08:03:57 +0800

    Create nick_ho2.py

---
+++ nick_ho2.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("MY PROFILE")
+window.geometry("600x600")
+window.resizable(True,True)
+window.configure(bg="lightblue",cursor="cross")
+
+label = tk.Label(window,text="MY PROFILE",font=("poppins",25),fg="black",bg="lightblue",anchor="center")
+label.pack(padx=5,pady=20)
+
+label = tk.Label(window,text="Name : NICK EDERZON M. QUEBRADO",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label.pack(anchor="w",pady=20)
+
+label = tk.Label(window,text="Age : 19 years old",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label.pack(anchor="w",pady=20)
+
+label = tk.Label(window,text="Course : BSIT",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label.pack(anchor="w",pady=20)
+
+label = tk.Label(window,text="Birthday : May 27,2006",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label.pack(anchor="w",pady=20)
+
+label = tk.Label(window,text="Motto :",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label.pack(anchor="w",pady=20)
+
+label = tk.Label(window,text="Never stop growing",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label.pack(padx=5,pady=20)
+
+window.mainloop()
