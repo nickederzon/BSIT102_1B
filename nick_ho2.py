858de7ec3b9f4f787b17d08be6d39fd1e6de2819
nickederzon <nickederzonmagnaye19@gmail.com>
2026-02-12 15:00:31 +0800

    Update nick_ho2.py

--- nick_ho2.py
+++ nick_ho2.py
@@ -10,22 +10,22 @@ window.configure(bg="lightblue",cursor="cross")
 label = tk.Label(window,text="MY PROFILE",font=("poppins",25),fg="black",bg="lightblue",anchor="center")
 label.pack(padx=5,pady=20)
 
-label = tk.Label(window,text="Name : NICK EDERZON M. QUEBRADO",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label = tk.Label(window,text="Name : NICK EDERZON M. QUEBRADO",font=("poppins",10),fg="black",bg="lightblue",anchor="center")
 label.pack(anchor="w",pady=20)
 
-label = tk.Label(window,text="Age : 19 years old",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label = tk.Label(window,text="Age : 19 years old",font=("poppins",10),fg="black",bg="lightblue",anchor="center")
 label.pack(anchor="w",pady=20)
 
-label = tk.Label(window,text="Course : BSIT",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label = tk.Label(window,text="Course : BSIT",font=("poppins",10),fg="black",bg="lightblue",anchor="center")
 label.pack(anchor="w",pady=20)
 
-label = tk.Label(window,text="Birthday : May 27,2006",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label = tk.Label(window,text="Birthday : May 27,2006",font=("poppins",10),fg="black",bg="lightblue",anchor="center")
 label.pack(anchor="w",pady=20)
 
-label = tk.Label(window,text="Motto :",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label = tk.Label(window,text="Motto :",font=("poppins",10),fg="black",bg="lightblue",anchor="center")
 label.pack(anchor="w",pady=20)
 
-label = tk.Label(window,text="Never stop growing",font=("poppins",8),fg="black",bg="lightblue",anchor="center")
+label = tk.Label(window,text="Never stop growing",font=("poppins",10),fg="black",bg="lightblue",anchor="center")
 label.pack(padx=5,pady=20)
 
 window.mainloop()
